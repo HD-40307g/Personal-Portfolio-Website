c27c909ee3e7578a4032dd38a46ec54741fc17f4
HD-40307g <152268341+HD-40307g@users.noreply.github.com>
2024-02-18 10:16:40 +0100

    Add JavaScript, README.md

---
+++ README.md
@@ -0,0 +1,26 @@
+# Personal Portfolio Website
+## Description
+This website is the personal portfolio website of Edward Crawford created for marketing purposes.
+This project was created according to instructions for the Challenge Project by CODECADEMY.
+
+## Features
+- Short article about myself.
+- Landing page with some of the projects I have worked on.
+- List of skills I am proficient in using.
+- Contact links and page.
+
+## How to use
+This is not an actual website and serves only for educational purposes.
+
+## Technologies
+- HTML
+- CSS
+- JavaScript
+- Git & GitHub
+- Command Line
+
+## Collaborators
+HD-40307g
+
+## License 
+GNU General Public License v3.0
---
+++ main.js
@@ -0,0 +1,121 @@
+const section = document.querySelector(".contact");
+const html = document.querySelector('#html');
+const css = document.querySelector('#css');
+const js = document.querySelector('#js');
+const nodejs = document.querySelector('#nodejs');
+const command = document.querySelector('#command-line');
+const react = document.querySelector('#react');
+const figma = document.querySelector('#figma');
+const git = document.querySelector('#git');
+const github = document.querySelector('#github');
+const mysql = document.querySelector('#mysql');
+const docker = document.querySelector('#docker');
+const vsc = document.querySelector('#vsc');
+const mail = document.querySelector('#mail');
+const GitHub = document.querySelector('#GitHub');
+const linkedin = document.querySelector('#linkedin');
+
+html.addEventListener("mouseenter", () => {
+    section.style.backgroundColor = "orange";
+});
+html.addEventListener("mouseleave", () => {
+    section.style.backgroundColor = "#f5f5dc";
+});
+
+css.addEventListener("mouseenter", () => {
+    section.style.backgroundColor = "blue";
+});
+css.addEventListener("mouseleave", () => {
+    section.style.backgroundColor = "#f5f5dc";
+});
+
+js.addEventListener("mouseenter", () => {
+    section.style.backgroundColor = "yellow";
+});
+js.addEventListener("mouseleave", () => {
+    section.style.backgroundColor = "#f5f5dc";
+});
+
+nodejs.addEventListener("mouseenter", () => {
+    section.style.backgroundColor = "green";
+});
+nodejs.addEventListener("mouseleave", () => {
+    section.style.backgroundColor = "#f5f5dc";
+});
+
+command.addEventListener("mouseenter", () => {
+    section.style.backgroundColor = "white";
+});
+command.addEventListener("mouseleave", () => {
+    section.style.backgroundColor = "#f5f5dc";
+});
+
+react.addEventListener("mouseenter", () => {
+    section.style.backgroundColor = "lightblue";
+});
+react.addEventListener("mouseleave", () => {
+    section.style.backgroundColor = "#f5f5dc";
+});
+
+figma.addEventListener("mouseenter", () => {
+    section.style.backgroundColor = "pink";
+});
+figma.addEventListener("mouseleave", () => {
+    section.style.backgroundColor = "#f5f5dc";
+});
+
+git.addEventListener("mouseenter", () => {
+    section.style.backgroundColor = "coral";
+});
+git.addEventListener("mouseleave", () => {
+    section.style.backgroundColor = "#f5f5dc";
+});
+
+github.addEventListener("mouseenter", () => {
+    section.style.backgroundColor = "darkslategray";
+});
+github.addEventListener("mouseleave", () => {
+    section.style.backgroundColor = "#f5f5dc";
+});
+
+mysql.addEventListener("mouseenter", () => {
+    section.style.backgroundColor = "cyan";
+});
+mysql.addEventListener("mouseleave", () => {
+    section.style.backgroundColor = "#f5f5dc";
+});
+
+docker.addEventListener("mouseenter", () => {
+    section.style.backgroundColor = "darkblue";
+});
+docker.addEventListener("mouseleave", () => {
+    section.style.backgroundColor = "#f5f5dc";
+});
+
+vsc.addEventListener("mouseenter", () => {
+    section.style.backgroundColor = "purple";
+});
+vsc.addEventListener("mouseleave", () => {
+    section.style.backgroundColor = "#f5f5dc";
+});
+
+mail.addEventListener("mouseenter", () => {
+    section.style.backgroundColor = "lightslategray";
+});
+mail.addEventListener("mouseleave", () => {
+    section.style.backgroundColor = "#f5f5dc";
+});
+
+GitHub.addEventListener("mouseenter", () => {
+    section.style.backgroundColor = "black";
+});
+GitHub.addEventListener("mouseleave", () => {
+    section.style.backgroundColor = "#f5f5dc";
+});
+
+linkedin.addEventListener("mouseenter", () => {
+    section.style.backgroundColor = "royalblue";
+});
+linkedin.addEventListener("mouseleave", () => {
+    section.style.backgroundColor = "#f5f5dc";
+});
